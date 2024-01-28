9172ce660d4b9fe4cdedb30100b924f040fc29de
jkdasari31 <jayakrishnardasari@gmail.com>
2024-01-28 16:28:32 +0530

    Initial Commit

--- 02-Variable.sh
+++ 02-Variable.sh
@@ -9,8 +9,16 @@ echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good $PERSON1, How are you"
 echo "$PERSON1: yeah, great"
 
-echo "Datatype : Date"
-
+echo "-------------------Type : Date----------------"
 DATE=$(date)
-
 echo "Script started executing: ${DATE}"
+
+echo "-------------------Type : Variable----------------"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"

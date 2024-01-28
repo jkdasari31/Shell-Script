fb72ddd6494a3381f84f24502f9dc8b6c24dcbad
jkdasari31 <jayakrishnardasari@gmail.com>
2024-01-28 16:22:42 +0530

    Initial Commit

---
+++ 02-Variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Akhil
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"
+echo "$PERSON1: yeah, great"

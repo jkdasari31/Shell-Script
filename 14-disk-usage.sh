d4b2aa5214d21839ee3458fb5623d667b09e062a
jkdasari31 <jayakrishnardasari@gmail.com>
2024-02-01 14:33:29 +0530

    Disk usage script testing

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -16,6 +16,6 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "High Disk Usage" jayakrishnardasari@gmail.com
+#echo "$message" | mail -s "High Disk Usage" jayakrishnardasari@gmail.com
 
-#sh mail.sh "DevOps Team" "High Disk Usage" "$message" "jayakrishnardasari@gmail.com" "ALERT High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "jayakrishnardasari@gmail.com" "ALERT High Disk Usage"
